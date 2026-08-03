1aecf846a2d9b7de0ad92f459ef03bc4e5053b09
Trisha Naik <trishanaik2006@gmail.com>
2026-08-03 19:50:22 +0530

    Time: 1731 ms (22.38%) | Memory: 19.7 MB (94.80%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+c=Solution()
+print(c.twoSum([1,8,4,5,7],9))
+
+        
